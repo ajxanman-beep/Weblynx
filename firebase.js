841f67aca8a532f8207d8b3c6cf2c57185e7a776
Retrick_aj <aj.xanman@gmail.com>
2026-01-09 06:52:47 -0600

    ts better fix

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const firebaseConfig = { /* your config */ };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
